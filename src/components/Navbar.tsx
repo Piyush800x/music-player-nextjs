32d361b029e83d1647006cd6a301b3b26cc3f1e0
shibammitra24 <shibammitra695@gmail.com>
2024-07-08 01:27:13 +0530

    [DRAFT] Added nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,38 @@
-import Link from "next/link"
-import { Input } from "@/components/ui/input"
-
+import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { MagnifyingGlassIcon, EnterIcon } from "@radix-ui/react-icons";
 
 export default function Navbar() {
-    return (
-        <div className="sticky backdrop-blur-xl bg-white/20 rounded-xl mx-24">
-            <div className="flex flex-row justify-between">
-                {/* Left Items */}
-                <div className="flex flex-row items-center gap-x-2 pl-2">
-                    <div><Link href='/'>Home</Link></div>
-                    <div><Link href='/'>Library</Link></div>
-                    <div><Link href='/'>Playlists</Link></div>
-                </div>
-                {/* Center */}
-                <div className="items-center "><Input type="search" placeholder="text"/></div>
-                {/* Right */}
-                <div className="flex items-center pr-2 ml-16">
-                    <div><Link href='/'>Sign In</Link></div>
-                </div>
-            </div>
+  return (
+    <div className="sticky bg-neutral-800 rounded-lg py-1.5 px-2.5 mx-32 min-w-">
+      <div className="flex flex-row justify-between items-center">
+        {/* Left Items */}
+        <div className="flex flex-row gap-6 font-semibold pl-2">
+          <div>
+            <Link href="/">Home</Link>
+          </div>
+          <div>
+            <Link href="/">Library</Link>
+          </div>
+          <div>
+            <Link href="/">Playlists</Link>
+          </div>
         </div>
-    )
+        {/* SEARCH*/}
+        <div className="items-center flex flex-row ">
+          <Link href="/" className="absolute z-50 ml-4">
+            <MagnifyingGlassIcon className="size-5"/>
+          </Link>
+          <Input type="search" placeholder="Search for musics" />
+        </div>
+        {/* Right */}
+        <div className="flex items-center pr-2 ml-16">
+          <div className="flex flex-row items-center gap-2">
+            <EnterIcon className="size-5"/>
+            <Link href="/" className="font-semibold">Sign In</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
