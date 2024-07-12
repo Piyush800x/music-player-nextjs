efc44424d259354c0aa6b344360b67aaf87ffae1
Piyush Paul <piyushpaul605@gmail.com>
2024-07-12 17:04:21 +0530

    fixed type problem

--- src/app/api/addmusic/route.ts
+++ src/app/api/addmusic/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import mongoClientPromise from "@/lib/mongodb";
 import { uploadToS3 } from "@/lib/aws";
+import { getArtistsCollection, getAlbumsCollection, getSongsCollection } from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
 
 
 export async function POST(req: NextRequest) {
@@ -22,39 +24,66 @@ export async function POST(req: NextRequest) {
             console.log(response);
         }
     }))
-    const song_id = await insertSong(data.musicName, data.artist, data.album, key);
+    const song_data = {
+        songName: data.musicName,
+        artistName: data.artist,
+        albumName: data.album,
+        songFileName: key
+    }
+    const song_id = await insertSong(song_data);
     console.log(`song_id: ${song_id}`);
 
     console.log(data)
     return NextResponse.json({status: 200});
 }
 
-const insertSong = async (songName: string, artistName: string, albumName: string, songFileName: string) => {
+interface SongData {
+    songName: string, 
+    artistName: string,
+    albumName: string,
+    songFileName: string
+}
+
+export async function insertSong({songName, artistName, albumName, songFileName}: SongData) {
     const client = await mongoClientPromise;
     const db = client.db(`${process.env.MONGODB_DB}`);
 
+    const artistsCollection = getArtistsCollection(db);
+    const albumsCollection = getAlbumsCollection(db);
+    const songsCollection = getSongsCollection(db);
+
     // Ensure if artist exists
-    let artist = await db.collection('artists').findOne({name: artistName});
+    let artist = await artistsCollection.findOne({name: artistName});
     if (!artist) {
-        const artistResult = await db.collection('artists').insertOne({name: artistName, albums: []});
+        const artistResult = await artistsCollection.insertOne({
+            _id: new ObjectId,
+            name: artistName, 
+            albums: []
+        });
         artist = {_id: artistResult.insertedId, name: artistName, albums: []};
     }
 
     // Ensure the album exists and is linked to the artist
-    let album = await db.collection('albums').findOne({name: albumName, artist_id: artist._id});
+    let album = await albumsCollection.findOne({name: albumName, artist_id: artist._id});
     if (!album) {
-        const albumResult = await db.collection('albums').insertOne({ name: albumName, artist_id: artist._id, songs: [] });
+        const albumResult = await albumsCollection.insertOne({
+            _id: new ObjectId,
+            name: albumName, 
+            artist_id: artist._id, 
+            songs: []
+        });
         album = { _id: albumResult.insertedId, name: albumName, artist_id: artist._id, songs: [] };
 
-        await db.collection('artists').updateOne(
+        await artistsCollection.updateOne(
             {_id: artist._id},
             { $push: { albums: { album_id: album._id, name: albumName } } }
         );
     }
 
     // inserting the song into songs collection
-    const songResult = await db.collection('songs').insertOne({
-        song_name: songName,
+    const songResult = await songsCollection.insertOne({
+        _id: new ObjectId,
+        name: songName,
         album_id: album._id,
         artist_id: artist._id,
         song_file_name: songFileName
@@ -62,9 +91,9 @@ const insertSong = async (songName: string, artistName: string, albumName: strin
     const song = {_id: songResult.insertedId, song_name: songFileName, song_file_name: songFileName}
 
     // Update the album to refer the new song
-    await db.collection('albums').updateOne(
+    await albumsCollection.updateOne(
         {_id: album._id},
-        { $push: { songs: { sond_id: song._id, song_file_name: songFileName}}}
+        { $push: { songs: { song_id: song._id, name: songName , song_file_name: songFileName}}}
     );
 
     return song._id
--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,5 +1,4 @@
-// lib/mongodb.ts
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId, Collection, Document, Db } from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please add your MongoDB URI to .env.local');
@@ -18,3 +17,28 @@ mongoClientPromise = client.connect();
 
 
 export default mongoClientPromise;
+
+export interface Artist {
+  _id: ObjectId,
+  name: string,
+  albums: Array<{album_id: ObjectId; name: string}>
+}
+
+export interface Album {
+  _id: ObjectId;
+  name: string;
+  artist_id: ObjectId;
+  songs: Array<{ song_id: ObjectId; name: string, song_file_name: string }>;
+}
+
+export interface Song {
+  _id: ObjectId;
+  name: string;
+  album_id: ObjectId;
+  artist_id: ObjectId;
+  song_file_name: string;
+}
+
+export const getArtistsCollection = (db: Db): Collection<Artist> => db.collection('artists');
+export const getAlbumsCollection = (db: Db): Collection<Album> => db.collection('albums');
+export const getSongsCollection = (db: Db): Collection<Song> => db.collection('songs');
